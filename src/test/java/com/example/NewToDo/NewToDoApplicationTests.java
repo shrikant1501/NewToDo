73953f58f2a545d0ff7d07543b79346ec42428fe
ShrikantThakre <thakresrikant@gmail.com>
2026-05-13 19:56:53 +0530

    setup

---
+++ src/test/java/com/example/NewToDo/NewToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NewToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
